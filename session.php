36a4b84e45802a61c20175b80e33684c346fa2bc
Cristian Leguizamon <37857113+pudinero@users.noreply.github.com>
2018-11-25 10:19:07 -0300

    Update session.php

--- session.php
+++ session.php
@@ -4,7 +4,7 @@
    
    $user_check = $_SESSION['login_user'];
    
-   $ses_sql = mysqli_query($db,"SELECT Usuario FROM Cliente WHERE Usuario = '$user_check' ");
+   $ses_sql = mysqli_query($db,"SELECT Usuario FROM Clientes WHERE Usuario = '$user_check' ");
    
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
