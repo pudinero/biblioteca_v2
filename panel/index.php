164e16d341d6d360998143f4b26aee18f59284f7
Cristian Leguizamon <37857113+pudinero@users.noreply.github.com>
2018-12-03 00:19:15 -0300

    Update index.php

--- panel/index.php
+++ panel/index.php
@@ -79,7 +79,7 @@
       <?php
         if($user->is_logged_in()){
           
-          $con  =mysqli_connect("localhost","root","","Biblioteca");
+          $con  =mysqli_connect("localhost","root","12345","Biblioteca");
           $stmt = $db->prepare('SELECT * FROM Clientes');
 					$stmt->execute(array(':username' => $_SESSION['username']));
           $row = $stmt->fetch(PDO::FETCH_ASSOC);
